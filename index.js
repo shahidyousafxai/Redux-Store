1b17e08bc225931b48d09005cfcefbcfea846d2b
Shahid Bahadur <shahidbahadur1998@gmail.com>
2022-03-02 15:31:55 +0500

    Middleware Added

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-import redux from 'redux'
+// Libraries
+import redux, { applyMiddleware, combineReducers } from 'redux'
+import reduxLogger from 'redux-logger'
+
+// Default Imports
 import { buyCake } from './ActionCreators.js'
 import { buyBiscuit } from './ActionCreators.js'
 import { CakeReducer } from './Reducer.js'
 import { biscuitReducer } from './Reducer.js'
 
-const reducer = redux.combineReducers({
+// combine Reducers
+const reducer = combineReducers({
   cake: CakeReducer,
   Biscuit: biscuitReducer,
 })
-const store = redux.createStore(reducer)
+
+// Logger
+const logger = reduxLogger.createLogger()
+
+// Store
+const store = redux.createStore(reducer, applyMiddleware(logger))
 console.log('Initial State', store.getState())
-// console.log(store)
-const unsubscribe = store.subscribe(() =>
-  console.log('Updated State', store.getState())
-)
-// store.subscribe(() => console.log('Updated State', store.getState()))
+const unsubscribe = store.subscribe(() => {})
+
+// Dispatch
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
